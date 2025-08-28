be68b59e8dcfa83162a7850265dc747c232dda59
Remy <remy.canal@live.fr>
2025-08-28 08:03:55 +0000

    Ajustement Background/interactive/verbeux

--- cont_start.sh
+++ cont_start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# start est fait en back ground sans sortie/erreur standard par defaut
 docker container start ${1:-first_container}
---
+++ cont_start_verbose.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker container start -a ${1:-first_container}
